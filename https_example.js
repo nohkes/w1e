073e6500828e2f492518d40870b0ffca71dd1349
Rawle Sekhon <amandeepsekhon404@gmail.com>
2018-09-30 21:26:35 +0000

    first commit

---
+++ https_example.js
@@ -0,0 +1,32 @@
+var https = require('https');
+
+console.log('I did it!');
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+// called by https when the request is made.
+var callback = function() {
+  console.log('In response handler callback!');
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+}
